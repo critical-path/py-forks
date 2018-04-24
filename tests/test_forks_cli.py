ef8a764d12d1e9e774b37c1cdd9c4310dc9bb9bc
critical-path <37241479+critical-path@users.noreply.github.com>
2018-04-23 22:08:02 -0400

    Add files via upload

--- tests/test_forks_cli.py
+++ tests/test_forks_cli.py
@@ -1,211 +1,362 @@
-from constants import (
-  API, 
-  FORKS, 
-  REPO0,
-  REPO1, 
-  REPOS,
-  REPO0_AND_FORKS,
-  REPOS_AND_FORKS,
-  USER
+from json import dumps
+
+from os import (
+    remove,
+    stat
 )
 
 from click.testing import CliRunner
 
 from forks.forks_cli import get_forks
 
-from json import dumps
-
 from responses import (
-  activate,
-  add,
-  GET
+    activate,
+    add,
+    GET
+)
+
+from constants import (
+    API,
+    FILENAME_USER,
+    FILENAME_USER_REPO,
+    FORKS,
+    REPO0,
+    REPO1,
+    REPOS,
+    REPO0_AND_FORKS,
+    REPOS_AND_FORKS,
+    USER
 )
 
 
 def test_cli_help():
-  runner = CliRunner()
+    runner = CliRunner()
 
-  result = runner.invoke(
-    get_forks,
-    [
-      "--help"
-    ]
-  )
+    result = runner.invoke(
+        get_forks,
+        [
+            "--help"
+        ]
+    )
 
-  assert result.exit_code == 0
-  assert "Usage" in result.output
-
-
-def test_cli_no_user_no_repo():
-  runner = CliRunner()
-
-  result = runner.invoke(
-    get_forks
-  )
-
-  assert result.exit_code != 0
-  assert "Missing" in result.output
+    assert result.exit_code == 0
+    assert "Usage" in result.output
 
 
-@activate
-def test_cli_user_repo_long():
-  endpoint = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
+def test_cli_no_user_no_repo_no_write():
+    runner = CliRunner()
 
-  add(
-    method=GET, 
-    url=endpoint, 
-    body=dumps(FORKS), 
-    status=200
-  )
+    result = runner.invoke(
+        get_forks
+    )
 
-  runner = CliRunner()
-
-  result = runner.invoke(
-    get_forks,
-    [
-      "--user", USER,
-      "--repo", REPO0
-    ]
-  )
-
-  assert result.exit_code == 0
-  assert result.output == dumps(REPO0_AND_FORKS, indent=2) + "\n"
+    assert result.exit_code != 0
+    assert "Missing" in result.output
 
 
 @activate
-def test_cli_user_repo_short():
-  endpoint = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
+def test_cli_user_repo_no_write_long():
+    endpoint = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
 
-  add(
-    method=GET, 
-    url=endpoint, 
-    body=dumps(FORKS), 
-    status=200
-  )
+    add(
+        method=GET,
+        url=endpoint,
+        body=dumps(FORKS),
+        status=200
+    )
 
-  runner = CliRunner()
+    runner = CliRunner()
 
-  result = runner.invoke(
-    get_forks,
-    [
-      "-u", USER,
-      "-r", REPO0
-    ]
-  )
+    result = runner.invoke(
+        get_forks,
+        [
+            "--user", USER,
+            "--repo", REPO0
+        ]
+    )
 
-  assert result.exit_code == 0
-  assert result.output == dumps(REPO0_AND_FORKS, indent=2) + "\n"
-
-
-@activate
-def test_cli_user_no_repo_long():
-  repos_endpoint = "{}/users/{}/repos".format(API, USER)
-
-  add(
-    method=GET,
-    url=repos_endpoint,
-    body=dumps(REPOS),
-    status=200
-  )
-
-  forks_endpoint0 = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
-
-  add(
-    method=GET,
-    url=forks_endpoint0,
-    body=dumps(FORKS),
-    status=200
-  )
-
-  forks_endpoint1 = "{}/repos/{}/{}/forks".format(API, USER, REPO1)
-
-  add(
-    method=GET,
-    url=forks_endpoint1,
-    body=dumps(FORKS),
-    status=200
-  )
-
-  runner = CliRunner()
-
-  result = runner.invoke(
-    get_forks,
-    [
-      "--user", USER
-    ]
-  )
-
-  assert result.exit_code == 0
-  assert result.output == dumps(REPOS_AND_FORKS, indent=2) + "\n"
+    assert result.exit_code == 0
+    assert result.output == dumps(REPO0_AND_FORKS, indent=2) + "\n"
 
 
 @activate
-def test_cli_user_no_repo_short():
-  repos_endpoint = "{}/users/{}/repos".format(API, USER)
+def test_cli_user_repo_no_write_short():
+    endpoint = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
 
-  add(
-    method=GET,
-    url=repos_endpoint,
-    body=dumps(REPOS),
-    status=200
-  )
+    add(
+        method=GET,
+        url=endpoint,
+        body=dumps(FORKS),
+        status=200
+    )
 
-  forks_endpoint0 = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
+    runner = CliRunner()
 
-  add(
-    method=GET,
-    url=forks_endpoint0,
-    body=dumps(FORKS),
-    status=200
-  )
+    result = runner.invoke(
+        get_forks,
+        [
+            "-u", USER,
+            "-r", REPO0
+        ]
+    )
 
-  forks_endpoint1 = "{}/repos/{}/{}/forks".format(API, USER, REPO1)
+    assert result.exit_code == 0
+    assert result.output == dumps(REPO0_AND_FORKS, indent=2) + "\n"
 
-  add(
-    method=GET,
-    url=forks_endpoint1,
-    body=dumps(FORKS),
-    status=200
-  )
 
-  runner = CliRunner()
+@activate
+def test_cli_user_no_repo_no_write_long():
+    repos_endpoint = "{}/users/{}/repos".format(API, USER)
 
-  result = runner.invoke(
-    get_forks,
-    [
-      "-u", USER
-    ]
-  )
+    add(
+        method=GET,
+        url=repos_endpoint,
+        body=dumps(REPOS),
+        status=200
+    )
 
-  assert result.exit_code == 0
-  assert result.output == dumps(REPOS_AND_FORKS, indent=2) + "\n"
+    forks_endpoint0 = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
+
+    add(
+        method=GET,
+        url=forks_endpoint0,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    forks_endpoint1 = "{}/repos/{}/{}/forks".format(API, USER, REPO1)
+
+    add(
+        method=GET,
+        url=forks_endpoint1,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        get_forks,
+        [
+            "--user", USER
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert result.output == dumps(REPOS_AND_FORKS, indent=2) + "\n"
+
+
+@activate
+def test_cli_user_no_repo_no_write_short():
+    repos_endpoint = "{}/users/{}/repos".format(API, USER)
+
+    add(
+        method=GET,
+        url=repos_endpoint,
+        body=dumps(REPOS),
+        status=200
+    )
+
+    forks_endpoint0 = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
+
+    add(
+        method=GET,
+        url=forks_endpoint0,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    forks_endpoint1 = "{}/repos/{}/{}/forks".format(API, USER, REPO1)
+
+    add(
+        method=GET,
+        url=forks_endpoint1,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        get_forks,
+        [
+            "-u", USER
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert result.output == dumps(REPOS_AND_FORKS, indent=2) + "\n"
+
+
+@activate
+def test_cli_user_repo_write_long():
+    endpoint = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
+
+    add(
+        method=GET,
+        url=endpoint,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        get_forks,
+        [
+            "--user", USER,
+            "--repo", REPO0,
+            "--write"
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert result.output == dumps(REPO0_AND_FORKS, indent=2) + "\n"
+    assert stat(FILENAME_USER_REPO)
+    remove(FILENAME_USER_REPO)
+
+
+@activate
+def test_cli_user_repo_write_short():
+    endpoint = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
+
+    add(
+        method=GET,
+        url=endpoint,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        get_forks,
+        [
+            "-u", USER,
+            "-r", REPO0,
+            "-w"
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert result.output == dumps(REPO0_AND_FORKS, indent=2) + "\n"
+    assert stat(FILENAME_USER_REPO)
+    remove(FILENAME_USER_REPO)
+
+
+@activate
+def test_cli_user_no_repo_write_long():
+    repos_endpoint = "{}/users/{}/repos".format(API, USER)
+
+    add(
+        method=GET,
+        url=repos_endpoint,
+        body=dumps(REPOS),
+        status=200
+    )
+
+    forks_endpoint0 = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
+
+    add(
+        method=GET,
+        url=forks_endpoint0,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    forks_endpoint1 = "{}/repos/{}/{}/forks".format(API, USER, REPO1)
+
+    add(
+        method=GET,
+        url=forks_endpoint1,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        get_forks,
+        [
+            "--user", USER,
+            "--write"
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert result.output == dumps(REPOS_AND_FORKS, indent=2) + "\n"
+    assert stat(FILENAME_USER)
+    remove(FILENAME_USER)
+
+
+@activate
+def test_cli_user_no_repo_write_short():
+    repos_endpoint = "{}/users/{}/repos".format(API, USER)
+
+    add(
+        method=GET,
+        url=repos_endpoint,
+        body=dumps(REPOS),
+        status=200
+    )
+
+    forks_endpoint0 = "{}/repos/{}/{}/forks".format(API, USER, REPO0)
+
+    add(
+        method=GET,
+        url=forks_endpoint0,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    forks_endpoint1 = "{}/repos/{}/{}/forks".format(API, USER, REPO1)
+
+    add(
+        method=GET,
+        url=forks_endpoint1,
+        body=dumps(FORKS),
+        status=200
+    )
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        get_forks,
+        [
+            "-u", USER,
+            "-w"
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert result.output == dumps(REPOS_AND_FORKS, indent=2) + "\n"
+    assert stat(FILENAME_USER)
+    remove(FILENAME_USER)
 
 
 def test_cli_repo_no_user_long():
-  runner = CliRunner()
+    runner = CliRunner()
 
-  result = runner.invoke(
-    get_forks,
-    [
-      "--repo", REPO0
-    ]
-  )
+    result = runner.invoke(
+        get_forks,
+        [
+            "--repo", REPO0
+        ]
+    )
 
-  assert result.exit_code != 0
-  assert "Missing" in result.output
+    assert result.exit_code != 0
+    assert "Missing" in result.output
 
 
 def test_cli_repo_no_user_short():
-  runner = CliRunner()
+    runner = CliRunner()
 
-  result = runner.invoke(
-    get_forks,
-    [
-      "-r", REPO0
-    ]
-  )
+    result = runner.invoke(
+        get_forks,
+        [
+            "-r", REPO0
+        ]
+    )
 
-  assert result.exit_code != 0
-  assert "Missing" in result.output
-
-
+    assert result.exit_code != 0
+    assert "Missing" in result.output
