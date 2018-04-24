b03fb4abd452e89a47fac89e4b771b81bfadfaeb
critical-path <37241479+critical-path@users.noreply.github.com>
2018-04-23 22:04:56 -0400

    Add files via upload

--- forks/forks_lib.py
+++ forks/forks_lib.py
@@ -2,81 +2,106 @@ from requests import get
 
 
 def get_repos(api, user):
-  """ Get list of all repos for a given user.
+    """Get list of all repos for a given user.
 
-  api : str
-      The API's fully-qualified domain name.
+    api : str
+        The API's fully-qualified domain name.
 
-  user : str
-      The user of interest.
+    user : str
+        The user of interest.
 
-  The parameters help to form the API endpoint.
-  """
+    The parameters help to form the API endpoint.
+    """
 
-  repos = []
+    repos = []
 
-  endpoint = "{}/users/{}/repos".format(api, user)
-  response = get(endpoint)
-  response_body = response.json()
+    endpoint = "{}/users/{}/repos".format(api, user)
+    response = get(endpoint)
+    response_body = response.json()
 
-  for element in response_body:
-    repo = element["name"]
-    repos.append(repo)
+    for element in response_body:
+        repo = element["name"]
+        repos.append(repo)
 
-  return repos
+    return repos
 
 
 def get_forks_for_one_repo(api, user, repo):
-  """ Get list of all forks for a given repo.
+    """Get list of all forks for a given repo.
 
-  Parameters
-  ----------
-  api : str
-      The API's fully-qualified domain name.
+    Parameters
+    ----------
+    api : str
+        The API's fully-qualified domain name.
 
-  user : str
-      The user of interest.
+    user : str
+        The user of interest.
 
-  repos : str
-      The repo of interest.
+    repos : str
+        The repo of interest.
 
-  The parameters help to form the API endpoint.
-  """
+    The parameters help to form the API endpoint.
+    """
 
-  forks = {}
-  
-  endpoint = "{}/repos/{}/{}/forks".format(api, user, repo)
-  response = get(endpoint)
-  response_body = response.json()
-  forks[repo] = response_body
+    forks = {}
 
-  return forks
-
-
-def get_forks_for_all_repos(api, user, repos):
-  """ Get list of all forks for a given list of repos.
-
-  Parameters
-  ----------
-  api : str
-      The API's fully-qualified domain name.
-
-  user : str
-      The user of interest.
-
-  repos : list
-      The repos of interest.
-
-  The parameters help to form the API endpoint.
-  """
-
-  forks = {}
-
-  for repo in repos:
     endpoint = "{}/repos/{}/{}/forks".format(api, user, repo)
     response = get(endpoint)
     response_body = response.json()
     forks[repo] = response_body
 
-  return forks
+    return forks
 
+
+def get_forks_for_all_repos(api, user, repos):
+    """Get list of all forks for a given list of repos.
+
+    Parameters
+    ----------
+    api : str
+        The API's fully-qualified domain name.
+
+    user : str
+        The user of interest.
+
+    repos : list
+        The repos of interest.
+
+    The parameters help to form the API endpoint.
+    """
+
+    forks = {}
+
+    for repo in repos:
+        endpoint = "{}/repos/{}/{}/forks".format(api, user, repo)
+        response = get(endpoint)
+        response_body = response.json()
+        forks[repo] = response_body
+
+    return forks
+
+
+def write_results(user=None, repo=None, forks=None):
+    """Write results to disk.
+
+    Parameters
+    ---------
+    user : str
+      The user of interest.
+
+    repo : str, optional
+      The repo of interest.
+
+    forks : str in JSON format
+      The forks associated with the user and repo.
+
+    The parameters help to form the filename.
+    """
+
+    if user and repo:
+        filename = "forks-{0}-{1}.json".format(user, repo)
+    elif user and not repo:
+        filename = "forks-{0}.json".format(user)
+
+    with open(filename, "w") as output:
+        output.write(forks)
