56e19b898928146ed720f901ff65fbca23f38b07
critical-path <37241479+critical-path@users.noreply.github.com>
2018-04-23 22:03:40 -0400

    Add files via upload

--- forks/forks_cli.py
+++ forks/forks_cli.py
@@ -1,15 +1,16 @@
-from click import (
-  command,
-  echo,
-  option
-)
-
 from json import dumps
 
+from click import (
+    command,
+    echo,
+    option
+)
+
 from forks.forks_lib import (
-  get_repos,
-  get_forks_for_one_repo,
-  get_forks_for_all_repos
+    get_repos,
+    get_forks_for_one_repo,
+    get_forks_for_all_repos,
+    write_results
 )
 
 
@@ -19,20 +20,24 @@ API = "https://api.github.com"
 @command()
 @option("--user", "-u", required=True, type=str, help="User of interest")
 @option("--repo", "-r", type=str, help="Repo of interest")
-def get_forks(user=None, repo=None):
-  """ Util that retrieves a list of forks for a given GitHub user and repo. """
+@option("--write", "-w", is_flag=True, help="Write results to disk")
+def get_forks(user=None, repo=None, write=False):
+    """Util that retrieves a list of forks for a given GitHub user and repo."""
 
-  if user and repo:
-    forks = get_forks_for_one_repo(API, user, repo)
+    if user and repo:
+        forks = get_forks_for_one_repo(API, user, repo)
 
-  elif user and not repo:
-    repos = get_repos(API, user)
-    forks = get_forks_for_all_repos(API, user, repos)
+    elif user and not repo:
+        repos = get_repos(API, user)
+        forks = get_forks_for_all_repos(API, user, repos)
 
-  forks = dumps(forks, indent=2)
-  echo(forks)
+    forks = dumps(forks, indent=2)
+
+    if write:
+        write_results(user=user, repo=repo, forks=forks)
+
+    echo(forks)
 
 
 if __name__ == "__main__":
-  get_forks()
-
+    get_forks()
