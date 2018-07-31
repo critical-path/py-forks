db56db700ce6265ab88ba041b3f37ec29bf26ea5
critical-path <37241479+critical-path@users.noreply.github.com>
2018-07-31 18:39:28 -0400

    Update forks_lib.py

--- forks/forks_lib.py
+++ forks/forks_lib.py
@@ -122,9 +122,10 @@ def write_results(user=None, repo=None, forks=None):
     N/A
     """
 
-    if user and repo:
+    if (user is not None) and (repo is not None):
         filename = "forks-{0}-{1}.json".format(user, repo)
-    elif user and not repo:
+
+    if (user is not None) and (repo is None):
         filename = "forks-{0}.json".format(user)
 
     with open(filename, "w") as output:
